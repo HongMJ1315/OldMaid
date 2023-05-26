109e8a0086315d882d4b620479632adce0f45470
HongMJ1315 <gone_co_1008@yahoo.com.tw>
2023-05-26 20:59:03 +0800

    Initial Commit

---
+++ OldMaid/OldMaidApp.swift
@@ -0,0 +1,17 @@
+//
+//  OldMaidApp.swift
+//  OldMaid
+//
+//  Created by Mr.JB on 2023/5/26.
+//
+
+import SwiftUI
+
+@main
+struct OldMaidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
